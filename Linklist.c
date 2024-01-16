0676b8d8054b69bf582ae0b5135ed97699189f21
Komal Narkhede <komalnarkhede2001@gmail.com>
2024-01-16 19:00:28 +0530

    Add initial File

---
+++ Linklist.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct Node
+{
+    int Data;
+    struct Node *next;
+};
+typedef struct Node NODE;
+typedef struct Node * PNODE;
+typedef struct Node ** PPNODE;
+int main()
+{
+    PNODE First = NULL;
+    return 0;
+}
